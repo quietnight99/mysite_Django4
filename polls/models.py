fd983cd3ac95eeee6235dd2fea58c4732b21f556
2231742813 <2231742813@qq.com>
2023-05-23 14:01:27 +0800

    202305231401

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,25 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class haha(models.Model):
+    hah1 = models.CharField(max_length = 10)
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_data = models.DateTimeField('data published')
-    pass
+    pub_date = models.DateTimeField('date published')
+    def __str__(self) :
+        return self.question_text
+
+        # 是否在当前发布的问卷
+    def was_published_recently(self) :
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
-    choice_txt = models.CharField(max_length = 200)
-    votes = models.IntegerField(default = 0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self) :
+        return self.choice_text
+
